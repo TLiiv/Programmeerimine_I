8f0fcf9e1c263302b2896493b28e4a2fa999d117
Tarmo <liivamagi.tarmo@gmail.com>
2024-11-05 14:27:10 +0200

    scaffolded crud working

--- KooliProjekt/Data/Leaderboard.cs
+++ KooliProjekt/Data/Leaderboard.cs
@@ -11,11 +11,11 @@ namespace KooliProjekt.Data
         [ForeignKey(nameof(User))]
         public Guid UserId { get; set; }
         public User? User { get; set; }
-        public ICollection<User> LeaderboardUsers { get; set; }
+        public ICollection<User>? LeaderboardUsers { get; set; }
 
         [ForeignKey(nameof(Tournament))]
         public Guid TournamentId { get; set; }
-        public Tournament Tournament { get; set; }
+        public Tournament? Tournament { get; set; }
 
         public int PredictedPoints { get; set; } = 0;
         public int Rank { get; set; } = 0;
