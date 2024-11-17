4293c4f146bda6aeb5588d5b43e39d134b0fa62c
Tarmo <liivamagi.tarmo@gmail.com>
2024-11-17 16:23:49 +0200

    db seeder users

---
+++ KooliProjekt/Data/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace KooliProjekt.Data
+{
+    public static class SeedData
+    {
+        public static void Generate(ApplicationDbContext context)
+        {
+
+
+            if (!context.Users.Any())
+            {
+                
+                var users = new List<User>
+                {
+                    new User
+                    {
+                        UserName = "AwesomeUser",
+                        FirstName = "Jaan",
+                        LastName = "Mustikas",
+                        Email = "jaan@mustikas.ee",
+                        Password = "password",
+                        PhoneNumber = "021987309",
+                        IsAdmin = false
+                    },
+                    new User
+                    {
+                        UserName = "CoolUser",
+                        FirstName = "Jaanus",
+                        LastName = "Kaalikas",
+                        Email = "jaanus@kaalikas.ee",
+                        Password = "password",
+                        PhoneNumber = "021987309",
+                        IsAdmin = false
+                    },
+                    new User
+                    {
+                        UserName = "LameUser",
+                        FirstName = "Kristjan",
+                        LastName = "Kuul",
+                        Email = "kristjan@kuul.ee",
+                        Password = "password",
+                        PhoneNumber = "021987309",
+                        IsAdmin = false
+                    },
+                    new User
+                    {
+                        UserName = "CheatingUser",
+                        FirstName = "Jaana",
+                        LastName = "Vaarikas",
+                        Email = "jaana@vaarikas.ee",
+                        Password = "password",
+                        PhoneNumber = "021987309",
+                        IsAdmin = false
+                    }
+                };
+
+                context.AddRange(users);
+                context.SaveChanges();
+            }
+        }
+    } 
+}
